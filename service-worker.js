e191018f0314990109a977bea306c4d2daae6851
supermanmubashir-png <superman.mubashir@gmail.com>
2026-03-18 15:03:02 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,1 @@
-const CACHE="quran-cache-v1000";
-self.addEventListener("install",e=>{self.skipWaiting(); e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./style.css","./app.js"])));});
-self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(names=>Promise.all(names.map(n=>{if(n!==CACHE) return caches.delete(n);}))))});
-self.addEventListener("fetch",e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));});
+self.addEventListener("fetch", e=>{});
